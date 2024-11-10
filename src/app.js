65f980b75d49a27c1c5f498913251f0d34750b8e
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2024-11-10 19:12:03 +1100

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,19 +2,35 @@ const express = require("express");
 
 // creates a server:
 const app = express();
+app.use(express.json());
 
 //Request handlers:
-app.get("/", (req, res) => {
-  res.send("Namaste Jagrut");
-});
-
-app.get("/fun", (req, res) => {
-  res.send("Having fun from server");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Hello from the servers");
-});
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Handling user 1");
+      // res.send("1st response!!");
+      // req.sampleVariable = "Hello hi namaste";
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling user 2");
+      // console.log(req.sampleVariable);
+      // res.send("2nd response!!");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling user 3");
+    // res.send("3rd response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling user 4");
+    res.send("4th response!!");
+  }
+);
 
 // listening on port 3000
 app.listen(3000, () => {
