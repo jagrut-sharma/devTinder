538034f4df99837d8cd398c8652bbcdd5d597834
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2024-11-14 21:02:15 +1100

    get APIs

--- src/app.js
+++ src/app.js
@@ -8,15 +8,37 @@ const app = express();
 app.use(express.json());
 
 // Request Handlers:
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send("Could not find the user");
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (!users.length) {
+      res.status(404).send("Unable to find Users");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    email: "virat@kohli.com",
-    password: "virat@123",
-    age: 31,
-    gender: "male",
-  });
+  console.log(req.body);
+
+  const user = new User(req.body);
 
   try {
     await user.save();
